e4ad353e14ab56da4a4fb6e0aa6f748fecd5ebd7
atikinvobud <dubov.n5@gmail.com>
2025-09-17 17:48:30 +0300

    Init FrontEnd

---
+++ FrontEnd/src/App.vue
@@ -0,0 +1,8 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+<template>
+  <h1 class="text-3xl font-bold underline text-blue-500 bg-gray-100 p-4 rounded-lg">
+    Hello Vue 3 + Tailwind CSS v4!
+  </h1>
+</template>
